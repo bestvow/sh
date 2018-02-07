26a8e7842522f649e98b4907cd453291edf1b4f9
bestvow <bestvow@foxmail.com>
2018-02-07 09:46:45 +0800

    update node installer fix rm tar file

--- node-install.sh
+++ node-install.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.local/bin;
 cd ~/.local/bin;
 wget http://nodejs.org/dist/latest-carbon/node-v8.9.4-linux-x64.tar.gz;
 tar xzvf node-v8.9.4-linux-x64.tar.gz;
-rm xzvf node-v8.9.4-linux-x64.tar.gz;
+rm node-v8.9.4-linux-x64.tar.gz;
 mv node-v8.9.4-linux-x64 nodejs;
 ln -s ~/.local/bin/nodejs/bin/node ~/.local/bin/node;
 ln -s ~/.local/bin/nodejs/bin/npm ~/.local/bin/npm;
